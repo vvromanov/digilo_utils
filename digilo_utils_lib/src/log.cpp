3c9c0732d12ef4b5bcfce315cfc64be0b0fb7b65
Vladimir Romanov <vromanov@gmail.com>
2017-06-11 19:38:21 +0300

    Fix segafault

--- digilo_utils_lib/src/log.cpp
+++ digilo_utils_lib/src/log.cpp
@@ -6,6 +6,7 @@
 
 bool log_to_syslog = true;
 bool log_to_console = true;
+static bool syslog_opened;
 uint64_t log_lines_count[LOG_MAX_LOG_LEVEL + 1];
 
 const char* log_level_to_name(int32_t level) {
@@ -34,24 +35,30 @@ const char* log_level_to_name(int32_t level) {
 
 void log_writev(int level, const char *format, va_list ap) {
     log_lines_count[level & LOG_PRIMASK]++;
-    if (log_to_syslog) {
-        if ((level & LOG_ERRNO)==LOG_ERRNO) {
-            char tmp[1024];
-            STRNCPY(tmp, format);
-            STRNCAT(tmp," | %m");
-            vsyslog(level & LOG_PRIMASK, tmp, ap);
-        } else {
-            vsyslog(level & LOG_PRIMASK, format, ap);
-        }
-    }
     if (log_to_console) {
+    	va_list ap1;
+    	__builtin_va_copy(ap1,ap);
         fprintf(stderr, "%s | ", log_level_to_name(level));
-        vfprintf(stderr,format, ap);
+        vfprintf(stderr,format, ap1);
         if ((level & LOG_ERRNO)==LOG_ERRNO) {
             fprintf(stderr, " | E%d - %s\n", errno, strerror(errno));
         } else {
             fputs("\n", stderr);
         }
+    }
+    if (log_to_syslog) {
+        if (!syslog_opened) {
+            syslog_opened = true;
+            openlog ("exampleprog", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+        }
+/*        if ((level & LOG_ERRNO) == LOG_ERRNO) {
+            static __thread char tmp[1024];
+            STRNCPY(tmp, format);
+            STRNCAT(tmp," | %m");
+            vsyslog(level & LOG_PRIMASK, tmp, ap);
+        } else {*/
+            vsyslog(level & LOG_PRIMASK, format, ap);
+        //}
     }
 }
 
