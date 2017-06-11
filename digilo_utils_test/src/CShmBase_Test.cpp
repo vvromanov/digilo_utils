9b02228f2d0f28c1847c6b5a1f53bfbe35487ba5
Vladimir Romanov <vromanov@gmail.com>
2017-06-11 14:31:27 +0300

    Add ShmBase.OpenInvalidSize test

--- digilo_utils_test/src/CShmBase_Test.cpp
+++ digilo_utils_test/src/CShmBase_Test.cpp
@@ -22,6 +22,13 @@ TEST(ShmBase, Open) {
     CShmBase test;
     EXPECT_TRUE(test.Open(TEST_SHM, TEST_SHM_SIZE));
     EXPECT_TRUE(is_file_exist("/dev/shm/"TEST_SHM));
-    EXPECT_NE((uint8_t*)NULL, test.GetData());
+    EXPECT_NE((uint8_t *) NULL, test.GetData());
     EXPECT_EQ(TEST_SHM_SIZE, test.GetSize());
+}
+
+TEST(ShmBase, OpenInvalidSize) {
+    CShmBase test;
+    EXPECT_TRUE(test.Open(TEST_SHM, TEST_SHM_SIZE));
+    test.Close();
+    EXPECT_FALSE(test.Open(TEST_SHM, TEST_SHM_SIZE+1));
 }
